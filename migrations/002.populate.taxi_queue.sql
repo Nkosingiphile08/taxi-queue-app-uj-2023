c54ebfc252056fa776b66bf916e65e8233ba3ca7
unknown <nkosingiphilejuliet@gmail.com>
2023-08-21 22:24:18 +0200

    Start script added in package.json file

--- migrations/002.populate.taxi_queue.sql
+++ migrations/002.populate.taxi_queue.sql
@@ -1,2 +1,5 @@
 insert into taxi_queue (passenger_queue_count, taxi_queue_count)
     values (0, 0);
+
+insert into taxi_queue (passenger_queue_count, taxi_queue_count)
+    values (12, 1);
--- taxi.sql.js
+++ taxi.sql.js
@@ -1,22 +1,21 @@
 import * as sqlite from 'sqlite';
 import sqlite3 from 'sqlite3';
 
-const  db = await sqlite.open({
-    filename:  './taxi_queue.db',
-    driver:  sqlite3.Database
+const db = await sqlite.open({
+    filename: './taxi_queue.db',
+    driver: sqlite3.Database
 });
 
 await db.migrate();
 
 export async function joinQueue() {
-  await db.run('UPDATE INTO passenger_queue (name) VALUES (?)', ['Person']);
-    // console.log('join queue')
+    await db.run('UPDATE passenger_queue SET name = ? WHERE id = ?', ['Person', 1]);
 }
 
 export async function leaveQueue() {
     const person = await db.get('SELECT * FROM passenger_queue ORDER BY id LIMIT 1');
     if (person) {
-        await db.run('UPDATE FROM passenger_queue WHERE id = ?', person.id);
+        await db.run('DELETE FROM passenger_queue WHERE id = ?', person.id);
     }
 }
 
@@ -26,15 +25,24 @@ export async function joinTaxiQueue() {
 
 export async function queueLength() {
     const result = await db.get('SELECT COUNT(*) AS count FROM passenger_queue');
-    return result.count;     
+    return result.count;
 }
 
 export async function taxiQueueLength() {
     const result = await db.get('SELECT COUNT(*) AS count FROM taxi_queue');
-    return result.count;  
-
+    return result.count;
 }
 
-export function taxiDepart() {
+export async function taxiDepart() {
+    const passengerCount = await queueLength();
+    const taxiQueueCount = await taxiQueueLength();
 
+    if (passengerCount >= 12 && taxiQueueCount > 0) {
+        // Remove 12 people from passenger queue
+        await db.run('REMOVE FROM passenger_queue WHERE id IN (SELECT id FROM passenger_queue LIMIT 12)');
+
+        // Remove one taxi from taxi queue
+        await db.run('REMOVE FROM taxi_queue WHERE id IN (SELECT id FROM taxi_queue LIMIT 1)');
+    }
 }
+
